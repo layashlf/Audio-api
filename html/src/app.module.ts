b7f3171e6ed77753a51e707fecd6884bb8389e76
Layash <layashsharma@gmail.com>
2026-01-17 09:16:48 +0000

    integrate Redis caching, Bull queuing, and new modules

--- html/src/app.module.ts
+++ html/src/app.module.ts
@@ -2,15 +2,47 @@ import { Module } from '@nestjs/common';
 
 import { SentryModule } from '@sentry/nestjs/setup';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { UserModule } from './user/user.module';
+import { AudioModule } from './audio/audio.module';
+import { SearchModule } from './search/search.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { envValidation } from './config/env.validation';
+import { HelperModule } from './utils/helper.module';
+import { BullModule } from '@nestjs/bullmq';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     SentryModule.forRoot(),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: 'nest-redis-server',
+            port: 6379,
+          },
+        }),
+      }),
+    }),
+    BullModule.forRoot({
+      connection: {
+        host: 'nest-redis-server',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'prompt-processing',
+    }),
     AuthenticationModule,
+    SubscriptionModule,
+    UserModule,
+    AudioModule,
+    SearchModule,
     PrismaModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
@@ -21,6 +53,7 @@ import { envValidation } from './config/env.validation';
       envFilePath: '.env',
       validate: envValidation,
     }),
+    HelperModule,
   ],
   controllers: [],
   providers: [],
