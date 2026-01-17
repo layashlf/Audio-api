5cb38f9468ff393b8af999a92c017cf2e7e3bee1
Layash <layashsharma@gmail.com>
2026-01-17 09:01:33 +0000

    add user entity

---
+++ html/src/authentication/entities/userEntity.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { UserStatus } from '@prisma/client';
+
+export class UserEntity {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  displayName: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  emailVerified: boolean = false;
+
+  @ApiProperty({ required: false, enum: UserStatus })
+  status?: UserStatus;
+
+  @ApiProperty()
+  createdAt: Date = new Date();
+
+  @ApiProperty()
+  updatedAt: Date = new Date();
+}
